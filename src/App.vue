c5543c0079e5b5ecda3356164525fb8391bd8496
wujian <406635565@qq.com>
2019-10-17 11:20:46 +0800

    引入bootstrap

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <hr>
+    <div class="btn-group" role="group" aria-label="...">
+      <button type="button" class="btn btn-primary">Left</button>
+      <button type="button" class="btn btn-success">Middle</button>
+      <button type="button" class="btn btn-danger">Right</button>
+    </div>
+    <hr>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
